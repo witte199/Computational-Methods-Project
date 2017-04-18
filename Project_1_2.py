c6de26bb35d221d59de1c4567497e5aade93786e
witte199 <witte199@umn.edu>
2017-04-18 14:09:46 -0500

    Add files via upload

---
+++ Project_1_2.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 23:13:11 2017
+
+@author: Libby
+"""
+
+#This is Project 1.2
+
+#CHANNEL MAP
+
+from PIL import Image
+
+import numpy as np
+import scipy as sp
+from scipy import ndimage
+#from numpy import ndarray
+
+
+import shutil
+import matplotlib.image as mpimg
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+from scipy.ndimage import gaussian_filter
+
+
+
+
+##define directory for accessing image
+#os.chdir("https://github.com/witte199/Computational-Methods-Project")
+
+
+
+#Open directory
+#a = open("C:\Users\Libby\Desktop\Binary_Images")
+
+##allows all photos in directory to be turned into binary images
+
+
+
+class project_1(object):
+    def __init__(self, name):
+        self.name = name
+    def listgen(self):
+        for x in range(10):
+            i=0
+            #identify files
+            filename = 'Img' + str(x+1) + ".jpg"
+            print filename
+##define image
+        im = mpimg.imread(filename)
+newlist = project_1("Img1_Img10")
+newlist.listgen()    
+
+def bitxt(listgen):
+
+#-------------------------------    
+    ##Identify shape of image
+    print im.shape
+
+    ##Identify size of image
+    print im.size
+
+    ##create pixelized image out of original image
+    mask = (im > im.mean()).astype(np.float)
+    label_im, nb_labels = ndimage.label(mask)
+
+    ##plot pixelized image
+    plt.imshow(label_im)
+
+    ##Separate "River" from "Not river"
+    hsv = matplotlib.colors.rgb_to_hsv(im)
+    river = hsv[:,:,0]-hsv[:,:,1] <-.5
+    river = hsv[:,:,1] > .55
+
+    ##Save data in folder
+    np.savetxt(str(filename) + ".txt", river, fmt = '%d')
+
+bitxt(newlist.listgen())    
